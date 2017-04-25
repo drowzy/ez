ef8ffc4f8cf2e6764c3ab7143820c9c4502c8ec3
Simon Thörnqvist <simon@plug-n-trade.com>
2017-04-25 10:55:43 +0200

    allow for compiling more expr

--- src/compiler.ml
+++ src/compiler.ml
@@ -5,9 +5,13 @@ let extract_value = function
   | Var i -> i
   | _ -> failwith "Unsupported"
 
-let compile = function
-  | EQ(Var el, Int er) -> `Assoc[ ("term", `Assoc[(el, `Int er)])]
-  | _ -> failwith "Unsupported for now"
+let rec compile = function
+  | Var i -> `String i
+  | Int i -> `Int i
+  | String s -> `String s
+  | Bool b -> `Bool b
+  | And(el, er) -> `Assoc[("must", `List[ (compile el); (compile er) ])]
+  | EQ(Var el, er) -> `Assoc[ ("term", `Assoc[(el, compile er)])]
 
 let to_string json =
   Yojson.Basic.pretty_to_string json
