cf4d4ffdb3cea4f3eba752123dff2a470cde221e
Simon Thörnqvist <simon@plug-n-trade.com>
2017-05-15 15:42:11 +0200

    wrap not in a bool expr

--- src/elastic_compiler.ml
+++ src/elastic_compiler.ml
@@ -15,9 +15,12 @@ let rec compile = function
   | String s -> `String s
   | Bool b -> `Bool b
   | And(el, er) -> bool_expr (`Assoc[("must", `List[ (compile el); (compile er) ])])
-  | Not(e) -> `Assoc[("must_not", compile e)]
+  | Not(e) -> bool_expr (`Assoc[("must_not", compile e)])
   | EQ(Var el, er) -> `Assoc[("term", `Assoc[(el, compile er)])]
   | EQ(Project(label, path), er) -> nested (`String label) (EQ(Var path, er) |> compile)
 
 let to_string json =
+  Yojson.Basic.to_string json
+
+let to_string_pretty json =
   Yojson.Basic.pretty_to_string json
