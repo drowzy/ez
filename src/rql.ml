7fa0b491c1da553b5bd33c5520bd5c93d04a2518
Simon Thörnqvist <simon@plug-n-trade.com>
2017-05-15 20:00:59 +0200

    cmd debug flag for outputing the original RQL query

--- src/rql.ml
+++ src/rql.ml
@@ -1,14 +1,20 @@
 open Core.Std
+open Yojson
 
 let get_inchan = function
   | "-" -> In_channel.stdin
   | filename -> In_channel.create ~binary: true filename
 
 (* compile ast to elastic json ast *)
-let compile filename =
-  get_inchan filename
-  |> Rql_parser.from_channel
+let compile str =
+  str
+  |> Rql_parser.from_string
   |> Elastic.Compiler.compile
+
+let maybe_append_query use_query ast =
+  match use_query with
+  | true -> Elastic.with_query ast
+  | false -> ast
 
 let cmd =
   Command.basic
@@ -16,14 +22,16 @@ let cmd =
     Command.Spec.(
       empty
       +> flag "-q" no_arg ~doc: "wraps output in a `query` object"
+      +> flag "-d" no_arg ~doc: "Returns a `debug` json string with the original RQL query inlined in the JSON output"
       +> anon (maybe_with_default "-" ("filename" %: file))
     )
-    (fun use_query filename () ->
-       let ast = compile filename in
-       let out = match use_query with
-       | true -> Elastic.with_query ast
-       | false -> ast in
-       out
+    (fun use_query debug filename () ->
+       let str = get_inchan filename |> In_channel.input_all in
+       let ast = compile str |> maybe_append_query use_query in
+       let res = match debug with
+         | true -> `Assoc[("rql", `String str); ("elastic", ast)]
+         | false -> ast in
+       res
        |> Elastic.to_string ~pretty: true
        |> print_endline
     )
