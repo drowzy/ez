61c0616d22fc435bc5f53ff3c8b1a4d005cc2ccb
Simon Thörnqvist <simon@plug-n-trade.com>
2017-05-17 13:36:18 +0200

    test loads correct files

--- test.ml
+++ test.ml
@@ -3,7 +3,7 @@
 #require "core";;
 #load "lexer.cmo";;
 #load "parser.cmo";;
-#load "elastic_compiler.cmo";;
-#load "rql_parser.cmo";;
+#load "elastic.cmo";;
+#load "ez_parser.cmo";;
 open Ast
-open Elastic_compiler
+open Elastic
