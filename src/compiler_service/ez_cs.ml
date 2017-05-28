edcef9d2390ebbd749e4e410c89ab0a70f534e32
Simon Thörnqvist <simon@plug-n-trade.com>
2017-05-28 02:35:24 +0200

    begin compiler_service

---
+++ src/compiler_service/ez_cs.ml
@@ -0,0 +1,22 @@
+open Opium.Std
+open Lwt.Infix
+
+(* TODO this is used like everywhere maybe its own lib instead of exposing everything everywhere? *)
+let ez_compile str =
+  str
+  |> Lexing.from_string
+  |> Ez_parser.prog Ez_lexer.read
+  |> Compiler.compile
+  |> Compiler.with_query
+  |> Compiler.to_string
+
+let compile = put "/compile" begin fun req ->
+    req
+    |> App.string_of_body_exn >>= fun res ->
+    `Json (Ezjsonm.from_string (ez_compile res)) |> respond'
+end
+
+let _ =
+  App.empty
+  |> compile
+  |> App.run_command
